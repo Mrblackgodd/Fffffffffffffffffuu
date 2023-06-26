88d18f2ffc5502465ce32a0b7eb1d069f46e102f
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2023-06-26 14:43:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+# Define your bot token
+TOKEN = 'YOUR_BOT_TOKEN'
+
+# Define the directory where files will be stored
+FILES_DIR = 'files'
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Welcome to the File Store Bot! Send me any file and I'll store it for you.")
+
+def store_file(update, context):
+    file = update.message.document
+    file_name = os.path.join(FILES_DIR, file.file_name)
+    file.get_file().download(file_name)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="File stored successfully!")
+
+def main():
+    # Create the files directory if it doesn't exist
+    os.makedirs(FILES_DIR, exist_ok=True)
+
+    # Create the Telegram Updater and dispatcher
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Register the handlers
+    start_handler = CommandHandler('start', start)
+    store_file_handler = MessageHandler(Filters.document, store_file)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(store_file_handler)
+
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
